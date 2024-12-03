7ba4f559f9d15c76eef74bd3a7e326eef0dbd49b
Marco Bacis <marc.bacis@gmail.com>
2024-12-03 16:12:54 +0100

    feat: aoc 2024 day 3

---
+++ 2024/src/bin/03.rs
@@ -0,0 +1,76 @@
+use std::fs;
+use regex::Regex;
+
+fn part_one(input: &str) -> Option<u32> {
+    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+
+    let mut results : Vec<(u32,u32)> = vec![];
+    for (_, [n1, n2]) in re.captures_iter(input).map(|c| c.extract()) {
+        results.push((n1.parse::<u32>().unwrap(), n2.parse::<u32>().unwrap()));
+    }
+
+    Some(results.iter().map(|(n1,n2)| n1 * n2).sum())
+
+}
+
+fn part_two(input: &str) -> Option<u32> {
+    let re = Regex::new(r"(don't\(\)|do\(\)|mul\(([0-9]+),([0-9]+)\))").unwrap();
+
+    let mut results : Vec<(u32,u32)> = vec![];
+    let mut enabled = true;
+
+    for c in re.captures_iter(input) {
+        
+        match c.get(0).map_or("", |m| m.as_str())  {
+          "do()" => { enabled = true; },
+          "don't()" =>  { enabled = false;} , 
+          &_ => { if(enabled) { results.push((c.get(2).unwrap().as_str().parse::<u32>().unwrap(), c.get(3).unwrap().as_str().parse::<u32>().unwrap()))} }
+        };
+
+    }
+    Some(results.iter().map(|(n1,n2)| n1 * n2).sum())
+}
+
+
+fn main() {
+    let input = fs::read_to_string("inputs/03.txt").unwrap();
+
+    println!("Solutions 🎄");
+    let result_part_one = part_one(&input);
+    let result_part_two = part_two(&input);
+
+    if let Some(res) = result_part_one {
+        println!("Part 1: {}", res);
+    }
+    if let Some(res) = result_part_two {
+        println!("Part 2: {}", res);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use rstest::rstest;
+
+    use super::*;
+
+    #[test]
+    fn part_one_test() {
+        let input = fs::read_to_string("examples/03.txt").unwrap();
+        assert_eq!(Some(161), part_one(&input));
+    }
+
+    #[rstest]
+    #[case("don't()mul(3,2)do()", Some(0))]
+    #[case("don't()do()mul(3,2)do()", Some(6))]
+    fn test_part_two_cases(#[case] input: &str, #[case] res: Option<u32>) {
+        assert_eq!(res, part_two(input));
+    }
+
+    #[test]
+    fn part_two_test() {
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(Some(48), part_two(&input));
+    }
+
+}
